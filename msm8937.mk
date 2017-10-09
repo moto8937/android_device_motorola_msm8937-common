dc0fbc63aa59814247f09df0f34f068eee83ec80
Sultan Qasim Khan <sultanqasim@gmail.com>
2017-10-08 20:13:49 -0400

    msm8937-common: Initial commit
    Change-Id: I9d189373d4a85f5aed5444d689d953fc24589c9b

---
+++ msm8937.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from qcom318-32
+$(call inherit-product, device/motorola/qcom318-32/qcom318-32.mk)
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.platform.rc
+
+$(call inherit-product-if-exists, vendor/motorola/msm8937-common/msm8937-common-vendor.mk)
